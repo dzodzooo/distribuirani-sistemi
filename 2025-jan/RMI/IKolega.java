1481ae9dfd5c9ca1331ef5943928a44e955e2b3e
dzodzooo <stojanovicjovana312@gmail.com>
2025-10-11 19:51:33 +0200

    fix: nismo radili JMS u novoj akreditaciji. Ovo su zadaci za novu akreditaciju za ovaj rok

--- 2025-jan/RMI/IKolega.java
+++
@@ -1,5 +0,0 @@
-import java.rmi.*;
-public interface IKolega extends Remote{
-	public void Print(String text) throws RemoteException;
-	public void PrimiPoruku(Poruka poruka) throws RemoteException;
-}
--- 2025-jan/RMI/ISistemZaOcenjivanje.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.*;
-
-public interface ISistemZaOcenjivanje extends Remote {
-	public void Pokreni(boolean nastavnik, String sifraPredmeta, IKolega kolega) throws RemoteException;
-	public void PosaljiPitanje(Poruka poruka) throws RemoteException;
-}
